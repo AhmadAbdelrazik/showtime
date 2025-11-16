d251e3da8ea214f119e2cd6d5fa778d143d4bdf6
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2025-11-16 20:11:46 +0200

    Added Abort with JSON to early return in case of an error in middleware

--- internal/controllers/middlewares.go
+++ internal/controllers/middlewares.go
@@ -14,33 +14,33 @@ func (h *Application) AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		value, err := c.Cookie("SESSION_ID")
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"unauthorized": "login credentials needed"})
 			return
 		}
 
 		userID := h.cache.Get(value)
 		if userID == "" {
-			c.JSON(http.StatusUnauthorized, nil)
 			c.SetCookie("SESSION_ID", "", -1, "/", "", false, false)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"unauthorized": "login credentials needed"})
 			return
 		}
 
 		// Should never fail since we store user ids in the cache.
 		id, err := strconv.ParseInt(userID, 10, 32)
 		if err != nil {
-			c.JSON(http.StatusUnauthorized, nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"unauthorized": "login credentials needed"})
 			return
 		}
 
 		// Should never fail except if the account has been deleted
-		user, err := h.models.Users.FindByID(int(id))
+		user, err := h.models.Users.Find(int(id))
 		if err != nil {
 			switch {
 			case errors.Is(err, models.ErrNotFound):
 				c.SetCookie("SESSION_ID", "", -1, "/", "", false, false)
-				c.JSON(http.StatusUnauthorized, gin.H{})
+				c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"unauthorized": "login credentials needed"})
 			default:
-				c.JSON(http.StatusInternalServerError, nil)
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{})
 			}
 			return
 		}
