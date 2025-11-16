b90dce2b7dc7c6b15fbda3d950f3ea4dd96d6bc0
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2025-11-16 20:12:23 +0200

    Encoded the Session ID to base32 for better usage

--- internal/controllers/helper.go
+++ internal/controllers/helper.go
@@ -1,10 +1,15 @@
 package controllers
 
-import "crypto/rand"
+import (
+	"crypto/rand"
+	"encoding/base32"
+)
 
 func (Application) generateRandomString() string {
 	b := make([]byte, 16)
 	rand.Read(b)
 
-	return string(b)
+	s := base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(b)
+
+	return s
 }
