b8950ea7e5e67153af7309517544ebf2ff6ecb1b
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2025-11-21 09:09:47 +0200

    added alphanum regex in validator package for hall codes

--- pkg/validator/regex.go
+++ pkg/validator/regex.go
@@ -3,9 +3,10 @@ package validator
 import "regexp"
 
 var (
-	EmailRX   = regexp.MustCompile(`^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$`)
-	LowerRX   = regexp.MustCompile(`[a-z]`)
-	UpperRX   = regexp.MustCompile(`[A-Z]`)
-	NumberRX  = regexp.MustCompile(`[0-9]`)
-	SpecialRX = regexp.MustCompile(`[!@#$%^&*]`)
+	EmailRX    = regexp.MustCompile(`^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$`)
+	LowerRX    = regexp.MustCompile(`[a-z]`)
+	AlphanumRX = regexp.MustCompile(`^[a-zA-Z0-9]+$`)
+	UpperRX    = regexp.MustCompile(`[A-Z]`)
+	NumberRX   = regexp.MustCompile(`[0-9]`)
+	SpecialRX  = regexp.MustCompile(`[!@#$%^&*]`)
 )
