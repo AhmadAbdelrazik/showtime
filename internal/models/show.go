696564e9fdff90cfe3604d2134f134f85a71422e
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2025-12-03 21:14:04 +0200

    Implemented Show Filter and Show search in ShowModel

--- internal/models/show.go
+++ internal/models/show.go
@@ -5,7 +5,11 @@ import (
 	"errors"
 	"fmt"
 	"log/slog"
+	"strings"
 	"time"
+
+	"github.com/AhmadAbdelrazik/showtime/pkg/validator"
+	sq "github.com/Masterminds/squirrel"
 )
 
 type Show struct {
@@ -24,6 +28,52 @@ type Show struct {
 
 type ShowModel struct {
 	db *sql.DB
+}
+
+func (m *ShowModel) Search(f ShowFilter) ([]Show, error) {
+	query, args, err := f.Build()
+	if err != nil {
+		slog.Error("filter build error", "filter", query)
+		return nil, err
+	}
+
+	rows, err := m.db.Query(query, args...)
+	if err != nil {
+		slog.Error("SQL Database Failure", "error", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	var shows []Show
+	for rows.Next() {
+		var show Show
+		err := rows.Scan(
+			&show.TheaterID,
+			&show.HallID,
+			&show.HallCode,
+			&show.MovieID,
+			&show.MovieTitle,
+			&show.MovieIMDBLink,
+			&show.StartTime,
+			&show.EndTime,
+			&show.CreatedAt,
+			&show.UpdatedAt,
+		)
+
+		if err != nil {
+			slog.Error("Scan Failure", "error", err)
+			return nil, err
+		}
+
+		shows = append(shows, show)
+	}
+
+	if err := rows.Err(); err != nil {
+		slog.Error("Scan Failure", "error", err)
+		return nil, err
+	}
+
+	return shows, nil
 }
 
 func (m *ShowModel) Create(show *Show) error {
@@ -118,4 +168,127 @@ func (m *ShowModel) Delete(id int) error {
 	}
 
 	return nil
+}
+
+type ShowFilter struct {
+	MovieTitle  *string    `form:"movie_title"`
+	TheaterName *string    `form:"theater_name"`
+	TheaterCity *string    `form:"theater_city"`
+	StartDate   *time.Time `form:"start_date"`
+	EndDate     *time.Time `form:"end_date"`
+	SortBy      *string    `form:"sort_by"`
+	Limit       *uint      `form:"limit"`
+	Offset      *uint      `form:"offset"`
+}
+
+func (f *ShowFilter) Validate(v *validator.Validator) {
+	if f.SortBy != nil {
+		validSortValues := map[string]string{
+			"movie_title":   "m.title",
+			"-movie_title":  "-m.title",
+			"theater_name":  "t.name",
+			"-theater_name": "-t.name",
+			"theater_city":  "t.city",
+			"-theater_city": "-t.city",
+			"date":          "s.start_time",
+			"-date":         "-s.start_time",
+		}
+		sort := *f.SortBy
+		if _, ok := validSortValues[sort]; !ok {
+			v.AddError("sort", "invalid sort value")
+		}
+	}
+
+	if f.Limit != nil {
+		v.Check(*f.Limit <= 100, "limit", "must be at most 100")
+	}
+
+	if f.MovieTitle != nil {
+		v.Check(len(*f.MovieTitle) <= 100, "movie_title", "must be at most 100 characters")
+	}
+
+	if f.TheaterName != nil {
+		v.Check(len(*f.TheaterName) <= 50, "theater_name", "must be at most 50 characters")
+	}
+
+	if f.TheaterCity != nil {
+		v.Check(len(*f.TheaterCity) <= 30, "theater_city", "must be at most 30 characters")
+	}
+}
+
+func (f *ShowFilter) Build() (string, []any, error) {
+	q := sq.Select(`h.theater_id, s.hall_id, h.code,
+		s.movie_id, m.title, m.imdb_link, s.start_time, s.end_time,
+		s.created_at, s.updated_at`).From(`shows AS s`).Join(`movies AS m on m.id =
+		s.movie_id`).Join(`halls AS h on h.id = s.hall_id`).Join(`theaters AS t on
+		t.id = h.theater_id`)
+
+	if f.MovieTitle != nil {
+		q = q.Where(sq.Expr(
+			"to_tsvector('english', m.title) @@ plainto_tsquery('english', ?)",
+			*f.MovieTitle,
+		))
+	}
+
+	if f.TheaterName != nil {
+		q = q.Where(sq.Expr(
+			"to_tsvector('english', t.name) @@ plainto_tsquery('english', ?)",
+			*f.TheaterName,
+		))
+	}
+
+	if f.TheaterCity != nil {
+		q = q.Where(sq.Expr(
+			"to_tsvector('english', t.city) @@ plainto_tsquery('english', ?)",
+			*f.TheaterCity,
+		))
+	}
+
+	if f.StartDate != nil {
+		q = q.Where("s.start_time > ?", *f.StartDate)
+	} else {
+		q = q.Where("s.start_time > NOW()")
+	}
+
+	if f.EndDate != nil {
+		q = q.Where("s.start_time < ?", *f.EndDate)
+	} else {
+		q = q.Where("s.start_time < ?", time.Now().Add(time.Hour*24*7))
+	}
+
+	if f.SortBy != nil {
+		validSortValues := map[string]string{
+			"movie_title":   "m.title",
+			"-movie_title":  "-m.title",
+			"theater_name":  "t.name",
+			"-theater_name": "-t.name",
+			"theater_city":  "t.city",
+			"-theater_city": "-t.city",
+			"date":          "s.start_time",
+			"-date":         "-s.start_time",
+		}
+		sort, ok := validSortValues[*f.SortBy]
+		if !ok {
+			panic("invalid sort value")
+		}
+
+		if strings.HasPrefix(sort, "-") {
+			sort, _ = strings.CutPrefix(sort, "-")
+			q = q.OrderBy(fmt.Sprint(sort, " DESC"))
+		} else {
+			q = q.OrderBy(sort)
+		}
+	}
+
+	if f.Limit != nil {
+		q = q.Limit(uint64(*f.Limit))
+	} else {
+		q = q.Limit(20)
+	}
+
+	if f.Offset != nil {
+		q = q.Offset(uint64(*f.Offset))
+	}
+
+	return q.PlaceholderFormat(sq.Dollar).ToSql()
 }
