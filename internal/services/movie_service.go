fcef60f63e3ebc72f0fcf7f83c625c7e8e090bcd
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2026-01-01 19:56:50 +0200

    Implemented Movie Find Service

--- internal/services/movie_service.go
+++ internal/services/movie_service.go
@@ -13,4 +13,23 @@ type MovieService struct {
 	provider MovieProvider
 }
 
+func (s *MovieService) Find(movieId string) (*models.Movie, error) {
+	movie, err := s.models.Movies.Find(movieId)
+	if err == nil {
+		return movie, nil
+	} else if err != models.ErrNotFound {
+		return movie, err
+	}
+
+	movie, err = s.provider.GetMovie(context.Background(), movieId)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.models.Movies.Create(movie); err != nil {
+		slog.Error(fmt.Sprintf("failed to store movie with id %v", movieId))
+		return movie, err
+	}
+
+	return movie, nil
 }
