8789d3696e87206539b3dd4891de2837772b7fce
Ahmad Abdelrazik <ahmadabdelrazik159@gmail.com>
2026-01-01 19:54:58 +0200

    Handled Failures for omdb client get movie request

--- internal/infrastructure/omdb/client.go
+++ internal/infrastructure/omdb/client.go
@@ -3,11 +3,15 @@ package omdb
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/AhmadAbdelrazik/showtime/internal/models"
+	"github.com/AhmadAbdelrazik/showtime/internal/services"
 )
+
+var ErrInvalidApiKey = errors.New("invalid api key")
 
 type Client struct {
 	apiKey string
@@ -22,15 +26,43 @@ func (c *Client) GetMovie(_ context.Context, movieId string) (*models.Movie, err
 	if err != nil {
 		return nil, err
 	}
-
 	defer resp.Body.Close()
 
-	var input movieResponse
+	var raw json.RawMessage
 
-	decoder := json.NewDecoder(resp.Body)
-	decoder.DisallowUnknownFields()
+	if err := json.NewDecoder(resp.Body).Decode(&raw); err != nil {
+		return nil, err
+	}
 
-	if err := decoder.Decode(&input); err != nil {
+	var success findSuccessResponse
+	if err := json.Unmarshal(raw, &success); err == nil && success.Response == "True" {
+		return &models.Movie{
+			ImdbID:     success.ImdbID,
+			Title:      success.Title,
+			Year:       success.Year,
+			Rated:      success.Rated,
+			Runtime:    success.Runtime,
+			Genre:      success.Genre,
+			Director:   success.Director,
+			Poster:     success.Poster,
+			ImdbRating: success.ImdbRating,
+		}, nil
+	}
+
+	var error errorResponse
+	if err := json.Unmarshal(raw, &error); err == nil && success.Response == "False" {
+		switch error.Error {
+		case "Incorrect IMDb ID.":
+			return nil, services.ErrInvalidMovieId
+		case "Invalid API key!":
+			return nil, ErrInvalidApiKey
+		default:
+			return nil, errors.New(error.Error)
+		}
+	}
+
+	return nil, errors.New("unknown response shape")
+}
 		return nil, err
 	}
 
--- internal/infrastructure/omdb/client_test.go
+++ internal/infrastructure/omdb/client_test.go
@@ -2,11 +2,13 @@ package omdb_test
 
 import (
 	"context"
+	"errors"
 	"reflect"
 	"testing"
 
 	"github.com/AhmadAbdelrazik/showtime/internal/infrastructure/omdb"
 	"github.com/AhmadAbdelrazik/showtime/internal/models"
+	"github.com/AhmadAbdelrazik/showtime/internal/services"
 )
 
 func TestClient_GetMovie(t *testing.T) {
@@ -15,9 +17,10 @@ func TestClient_GetMovie(t *testing.T) {
 		// Named input parameters for receiver constructor.
 		apiKey string
 		// Named input parameters for target function.
-		movieId string
-		want    *models.Movie
-		wantErr bool
+		movieId   string
+		want      *models.Movie
+		wantErr   bool
+		wantedErr error
 	}{
 		// TODO: Add test cases.
 		{
@@ -35,7 +38,24 @@ func TestClient_GetMovie(t *testing.T) {
 				Poster:     "https://m.media-amazon.com/images/M/MV5BMzA2NDQzZDEtNDU5Ni00YTlkLTg2OWEtYmQwM2Y1YTBjMjFjXkEyXkFqcGc@._V1_SX300.jpg",
 				ImdbRating: "8.2",
 			},
-			wantErr: false,
+			wantErr:   false,
+			wantedErr: nil,
+		},
+		{
+			name:      "invalid movie id",
+			apiKey:    "20d1919",
+			movieId:   "tt03724",
+			want:      nil,
+			wantErr:   true,
+			wantedErr: services.ErrInvalidMovieId,
+		},
+		{
+			name:      "invalid api key",
+			apiKey:    "20919",
+			movieId:   "tt0372784",
+			want:      nil,
+			wantErr:   true,
+			wantedErr: omdb.ErrInvalidApiKey,
 		},
 	}
 	for _, tt := range tests {
@@ -45,16 +65,20 @@ func TestClient_GetMovie(t *testing.T) {
 			if gotErr != nil {
 				if !tt.wantErr {
 					t.Errorf("GetMovie() failed: %v", gotErr)
+				} else if !errors.Is(gotErr, tt.wantedErr) {
+					t.Errorf("GetMovie() error mismatch\n"+
+						"got: %v\n"+"want: %v\n", gotErr, tt.wantedErr)
 				}
 				return
 			}
 			if tt.wantErr {
 				t.Fatal("GetMovie() succeeded unexpectedly")
 			}
-			// TODO: update the condition below to compare got with tt.want.
+
 			if reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GetMovie() = %v, want %v", got, tt.want)
 			}
 		})
 	}
+
 }
